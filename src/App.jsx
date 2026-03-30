865730f5b05c1805d2a4b003bb4e8d3a6b8b8613
Maryna Kapula <maryna.kapula@gmail.com>
2026-03-30 13:26:18 +0300

    7.3 valmis

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
 import { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useParams,
+  useNavigate,
+} from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -7,9 +15,9 @@ const Menu = () => {
 
   return (
     <div>
-      <a href="#" style={padding}>anecdotes</a>
-      <a href="#" style={padding}>create new</a>
-      <a href="#" style={padding}>about</a>
+      <Link style={padding} to="/">anecdotes</Link>
+      <Link style={padding} to="/create">create new</Link>
+      <Link style={padding} to="/about">about</Link>
     </div>
   )
 }
@@ -20,12 +28,30 @@ const AnecdoteList = ({ anecdotes }) => (
     <ul>
       {anecdotes.map(a =>
         <li key={a.id}>
-          {a.content}
+          <Link to={`/anecdotes/${a.id}`}>{a.content}</Link>
         </li>
       )}
     </ul>
   </div>
 )
+
+const Anecdote = ({ anecdotes }) => {
+  const { id } = useParams()
+  const anecdote = anecdotes.find(a => a.id === Number(id))
+
+  if (!anecdote) {
+    return <div>Anecdote not found</div>
+  }
+
+  return (
+    <div>
+      <h2>{anecdote.content}</h2>
+      <div>author: {anecdote.author}</div>
+      <div>info: <a href={anecdote.info}>{anecdote.info}</a></div>
+      <div>votes: {anecdote.votes}</div>
+    </div>
+  )
+}
 
 const About = () => (
   <div>
@@ -39,12 +65,56 @@ const About = () => (
 
 const Footer = () => (
   <div>
+    <br />
     Anecdote app for Full Stack Open.
   </div>
 )
 
+const CreateNew = ({ addNew }) => {
+  const navigate = useNavigate()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const content = event.target.content.value
+    const author = event.target.author.value
+    const info = event.target.info.value
+
+    addNew({
+      content,
+      author,
+      info,
+      votes: 0,
+    })
+
+    event.target.reset()
+    navigate('/')
+  }
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input name="content" />
+        </div>
+        <div>
+          author
+          <input name="author" />
+        </div>
+        <div>
+          url for more info
+          <input name="info" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
 const App = () => {
-  const [anecdotes] = useState([
+  const [anecdotes, setAnecdotes] = useState([
     {
       content: 'If it hurts, do it more often',
       author: 'Jez Humble',
@@ -61,11 +131,46 @@ const App = () => {
     },
   ])
 
+  const [notification, setNotification] = useState('')
+
+  const addNew = (anecdote) => {
+    anecdote.id = (Math.random() * 10000).toFixed(0)
+    setAnecdotes(anecdotes.concat(anecdote))
+    setNotification(`a new anecdote '${anecdote.content}' created`)
+    setTimeout(() => {
+      setNotification('')
+    }, 5000)
+  }
+
   return (
     <div>
       <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
+
+      <Router>
+        <Menu />
+
+        {notification && <div>{notification}</div>}
+
+        <Routes>
+          <Route
+            path="/anecdotes/:id"
+            element={<Anecdote anecdotes={anecdotes} />}
+          />
+          <Route
+            path="/create"
+            element={<CreateNew addNew={addNew} />}
+          />
+          <Route
+            path="/about"
+            element={<About />}
+          />
+          <Route
+            path="/"
+            element={<AnecdoteList anecdotes={anecdotes} />}
+          />
+        </Routes>
+      </Router>
+
       <Footer />
     </div>
   )
